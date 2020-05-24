ff9494b2ec5b97925336ee6552909b780280f3f8
Zyper <zyperpl@gmail.com>
2020-05-24 11:57:47 +0200

    BufferBuilder: Simple objects and std::string serialization

---
+++ src/ZD/BufferBuilder.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <vector>
+
+class BufferBuilder
+{
+public:
+  template<typename D>
+  size_t add(const D &value)
+  {
+    buffer.resize(buffer.size() + sizeof(D));
+    memcpy(buffer.data() + buffer.size() - sizeof(D), &value, sizeof(D));
+    return sizeof(D);
+  }
+
+  [[nodiscard]] std::vector<uint8_t> get_buffer() const { return buffer; }
+
+private:
+  std::vector<uint8_t> buffer;
+};
+
+template<>
+size_t BufferBuilder::add<std::string>(const std::string &value)
+{
+  const auto &size = sizeof(char) * value.size() + 1;
+  buffer.resize(buffer.size() + size);
+  memcpy(buffer.data() + buffer.size() - size, value.data(), size);
+  return size;
+}
